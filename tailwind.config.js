910af5b7df1de5edbdd7431af8b9d684c43b3a63
emilinasanduleac <e.codreanu@students.clark.edu>
2024-01-12 21:22:34 -0600

    addedn logo in nav bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ['./*.{html,js}'],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      colors: {
+        blue: '#60a5fa',
+        gray:'#6b7280'
+      }
+    },
   },
   plugins: [],
 }
